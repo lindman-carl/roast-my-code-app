dc52bc40fd124f0ef5a8ecd29256d5cf5928f263
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-08-16 00:22:17 +0200

    init prettier with tailwind plugin

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ const App = () => {
   return (
     <div
       id="app-contianer"
-      className="w-full h-screen justify-center flex items-center bg-teal-600 text-white"
+      className="flex h-screen w-full items-center justify-center bg-teal-600 text-white"
     >
-      <div className="w-full max-w-2xl p-16 flex justify-center items-center flex-col shadow-lg bg-teal-900">
+      <div className="flex w-full max-w-2xl flex-col items-center justify-center bg-teal-900 p-16 shadow-lg">
         <h1 className="text-4xl font-bold">Roast my code</h1>
         <div>app.</div>
       </div>
