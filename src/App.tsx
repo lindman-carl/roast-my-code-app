ee0b052cf6593056ab5d8b3a235adfcfbf406e92
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-08-16 00:11:40 +0200

    init tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import {} from 'react';
+import {} from "react";
 
 const App = () => {
-	return (
-		<div id="app-contianer">
-			<h1>Roast my code</h1>
-			<div>app.</div>
-		</div>
-	);
+  return (
+    <div
+      id="app-contianer"
+      className="w-full h-screen justify-center flex items-center bg-teal-600 text-white"
+    >
+      <div className="w-full max-w-2xl p-16 flex justify-center items-center flex-col shadow-lg bg-teal-900">
+        <h1 className="text-4xl font-bold">Roast my code</h1>
+        <div>app.</div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
